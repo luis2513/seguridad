09423da3ff333aebc613d0d63cb7df014a11f06e
Asus <waltri.2513@hotmail.com>
2023-05-14 18:04:13 -0500

    security

---
+++ src/main/java/med/voll/api/domain/medico/DatosListadoMedico.java
@@ -0,0 +1,8 @@
+package med.voll.api.domain.medico;
+
+public record DatosListadoMedico(Long id, String nombre, Especialidad especialidad, String documento, String email) {
+
+    public DatosListadoMedico (Medico medico){
+        this(medico.getId(), medico.getNombre(), medico.getEspecialidad(), medico.getDocumento(), medico.getEmail());
+    }
+}
---
+++ src/main/resources/db/migration/V5__create-table-usuarios-add-usuario.sql
@@ -0,0 +1,1 @@
+insert into usuarios values(1, 'jeimmy', '$2a$10$7Uuof7l.bc8NwbzFgqy8QummvZD6Lvxet65splDth2aEioQBw0Idu');
